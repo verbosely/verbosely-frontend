accd41eaed8230262f499ff050d71a1496c35232
Zachary Flohr <zachary.flohr@case.edu>
2023-01-05 07:58:58 -0500

    Modify mongoengine document model for mongodb

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,10 +1,6 @@
 # Import third-party modules, classes, and functions
 from mongoengine import * 
 
-class Orthography(EmbeddedDocument):
-    singular = ListField(StringField())
-    plural = ListField(StringField())
-
 class Orthoepy(EmbeddedDocument):
     singular = ListField(StringField())
     plural = ListField(StringField())
@@ -13,9 +9,9 @@ class BaseWordDocument(Document):
     main_vocab = BooleanField()
     minor_vocab = BooleanField()
     part_of_speech = StringField()
-    orthography = EmbeddedDocumentField(Orthography)
+    orthography = DictField()
     orthoepy = EmbeddedDocumentField(Orthoepy)
-    meanings = ListField(StringField())
+    meanings = DictField()
     abbreviations = DictField()
     hyperlinks = DictField()
     transliterations = DictField()
