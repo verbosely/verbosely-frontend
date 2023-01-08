98dc1f975f8405cbb5e3f3f6952a2bc75bc87bce
Zachary Flohr <zachary.flohr@case.edu>
2023-01-08 05:34:30 -0500

    Create template component for child of root route's component
    - Create template React component that will be rendered inside the root
      route's component when axios returns a 200 status code for requests
      for lexicographic data that are sent to nginx. The template component
      will be the component responsible for data visualization.

---
+++ frontend/src/pages/wordData.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WordData extends Component {
+   render() {
+      return(
+	 <p>test test test</p>
+      );
+   }
+}
+
+export default WordData;
