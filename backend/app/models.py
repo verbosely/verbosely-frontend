708ade229112f164b3c7b5c8b0933a2c0e346ff8
Zachary Flohr <zachary.flohr@case.edu>
2023-01-26 03:26:16 -0500

    Add structure to value of base class public attribute
    - Require certain keys in the dictionary that is the value of the
      orthography attribute of the base class of the document models by
      converting DictField to EmbeddedDocumentField. This prevents misspelled
      keys from being stored in the MongoDB database, which could otherwise
      lead to inaccurate query results.

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,6 +2,16 @@
 import flask_mongoengine
 from mongoengine import *
 
+class OrthographyWithAlternatives(EmbeddedDocument):
+    preferred = ListField(StringField())
+    alternatives = ListField(StringField())
+
+class Orthography(EmbeddedDocument):
+    singular = ListField(StringField(), default=None)
+    plural = ListField(StringField(), default=None)
+    singular_with_alternatives = EmbeddedDocumentField(OrthographyWithAlternatives)
+    plural_with_alternatives = EmbeddedDocumentField(OrthographyWithAlternatives)
+
 class BaseWordDocument(flask_mongoengine.Document):
     abbreviations = DictField()
     bold = DictField()
@@ -15,7 +25,7 @@ class BaseWordDocument(flask_mongoengine.Document):
     meta = {'abstract': True}
     minor_vocab = BooleanField()
     orthoepy = DictField()
-    orthography = DictField()
+    orthography = EmbeddedDocumentField(Orthography)
     part_of_speech = StringField()
     phrases = DictField()
     quotations = DictField()
