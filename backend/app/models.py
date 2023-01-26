25a62e330675d457c837ef40d557f52a2c7b42aa
Zachary Flohr <zachary.flohr@case.edu>
2023-01-26 17:22:59 -0500

    Remove wildcard import pursuant to PEP 8

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,75 +1,84 @@
-# Import third-party modules, classes, and functions
-import flask_mongoengine
-from mongoengine import *
+# Third party imports
+import mongoengine as me
 
-class Level2StartIndex(EmbeddedDocument):
-    no_1 = ListField(IntField(), default=None)
-    no_2 = ListField(IntField(), default=None) 
-    no_3 = ListField(IntField(), default=None) 
+class Level2StartIndex(me.EmbeddedDocument):
+    no_1 = me.ListField(me.IntField(), default=None)
+    no_2 = me.ListField(me.IntField(), default=None) 
+    no_3 = me.ListField(me.IntField(), default=None) 
 
-class Level1StartIndex(EmbeddedDocument):
-    meaning = ListField(IntField(), default=None) 
-    a = ListField(IntField(), default=None)  
-    b = ListField(IntField(), default=None)  
-    c = ListField(IntField(), default=None)  
-    d = ListField(IntField(), default=None)  
-    e = ListField(IntField(), default=None)  
-    f = ListField(IntField(), default=None) 
-    g = ListField(IntField(), default=None) 
-    h = ListField(IntField(), default=None)  
-    i = ListField(IntField(), default=None)
-    meaning_with_levels = EmbeddedDocumentField(Level2StartIndex)
-    a_with_levels = EmbeddedDocumentField(Level2StartIndex) 
-    b_with_levels = EmbeddedDocumentField(Level2StartIndex) 
-    c_with_levels = EmbeddedDocumentField(Level2StartIndex) 
-    d_with_levels = EmbeddedDocumentField(Level2StartIndex) 
-    e_with_levels = EmbeddedDocumentField(Level2StartIndex) 
-    f_with_levels = EmbeddedDocumentField(Level2StartIndex) 
-    g_with_levels = EmbeddedDocumentField(Level2StartIndex) 
-    h_with_levels = EmbeddedDocumentField(Level2StartIndex) 
-    i_with_levels = EmbeddedDocumentField(Level2StartIndex) 
+class Level1StartIndex(me.EmbeddedDocument):
+    meaning = me.ListField(me.IntField(), default=None) 
+    a = me.ListField(me.IntField(), default=None)  
+    b = me.ListField(me.IntField(), default=None)  
+    c = me.ListField(me.IntField(), default=None)  
+    d = me.ListField(me.IntField(), default=None)  
+    e = me.ListField(me.IntField(), default=None)  
+    f = me.ListField(me.IntField(), default=None) 
+    g = me.ListField(me.IntField(), default=None) 
+    h = me.ListField(me.IntField(), default=None)  
+    i = me.ListField(me.IntField(), default=None)
+    meaning_with_levels = me.EmbeddedDocumentField(Level2StartIndex)
+    a_with_levels = me.EmbeddedDocumentField(Level2StartIndex) 
+    b_with_levels = me.EmbeddedDocumentField(Level2StartIndex) 
+    c_with_levels = me.EmbeddedDocumentField(Level2StartIndex) 
+    d_with_levels = me.EmbeddedDocumentField(Level2StartIndex) 
+    e_with_levels = me.EmbeddedDocumentField(Level2StartIndex) 
+    f_with_levels = me.EmbeddedDocumentField(Level2StartIndex) 
+    g_with_levels = me.EmbeddedDocumentField(Level2StartIndex) 
+    h_with_levels = me.EmbeddedDocumentField(Level2StartIndex) 
+    i_with_levels = me.EmbeddedDocumentField(Level2StartIndex) 
 
-class StartIndex(EmbeddedDocument):
-    meaning_1 = ListField(IntField(), default=None)
-    meaning_2 = ListField(IntField(), default=None)
-    meaning_3 = ListField(IntField(), default=None)
-    meaning_1_with_levels = EmbeddedDocumentField(Level1StartIndex)
-    meaning_2_with_levels = EmbeddedDocumentField(Level1StartIndex)
-    meaning_3_with_levels = EmbeddedDocumentField(Level1StartIndex)
+class StartIndex(me.EmbeddedDocument):
+    meaning_1 = me.ListField(me.IntField(), default=None)
+    meaning_2 = me.ListField(me.IntField(), default=None)
+    meaning_3 = me.ListField(me.IntField(), default=None)
+    meaning_1_with_levels = me.EmbeddedDocumentField(Level1StartIndex)
+    meaning_2_with_levels = me.EmbeddedDocumentField(Level1StartIndex)
+    meaning_3_with_levels = me.EmbeddedDocumentField(Level1StartIndex)
 
-class StringLocations(EmbeddedDocument):
-    string = StringField()
-    start_index = EmbeddedDocumentField(StartIndex)
+class StringLocations(me.EmbeddedDocument):
+    string = me.StringField()
+    start_index = me.EmbeddedDocumentField(StartIndex)
 
-class OrthographyWithAlternatives(EmbeddedDocument):
-    preferred = ListField(StringField())
-    alternatives = ListField(StringField())
+class UniqueOccurrences(me.EmbeddedDocument):
+    no_1 = me.EmbeddedDocumentField(StringLocations)
+    no_2 = me.EmbeddedDocumentField(StringLocations) 
+    no_3 = me.EmbeddedDocumentField(StringLocations) 
+    no_4 = me.EmbeddedDocumentField(StringLocations) 
+    no_5 = me.EmbeddedDocumentField(StringLocations) 
+    no_6 = me.EmbeddedDocumentField(StringLocations) 
+    no_7 = me.EmbeddedDocumentField(StringLocations) 
+    no_8 = me.EmbeddedDocumentField(StringLocations) 
 
-class Orthography(EmbeddedDocument):
-    singular = ListField(StringField(), default=None)
-    plural = ListField(StringField(), default=None)
-    singular_with_alternatives = EmbeddedDocumentField(OrthographyWithAlternatives)
-    plural_with_alternatives = EmbeddedDocumentField(OrthographyWithAlternatives)
+class OrthographyWithAlternatives(me.EmbeddedDocument):
+    preferred = me.ListField(me.StringField())
+    alternatives = me.ListField(me.StringField())
 
-class BaseWordDocument(flask_mongoengine.Document):
-    abbreviations = EmbeddedDocumentField(StringLocations)
-    bold = DictField()
-    capital_required = DictField()
-    cross_references = DictField()
-    exclusiveness = DictField()
-    inline_audio = DictField()
-    italics = DictField()
-    main_vocab = BooleanField()
-    meanings = DictField()
+class Orthography(me.EmbeddedDocument):
+    singular = me.ListField(me.StringField(), default=None)
+    plural = me.ListField(me.StringField(), default=None)
+    singular_with_alternatives = me.EmbeddedDocumentField(OrthographyWithAlternatives)
+    plural_with_alternatives = me.EmbeddedDocumentField(OrthographyWithAlternatives)
+
+class BaseWordDocument(me.Document):
+    abbreviations = me.EmbeddedDocumentField(UniqueOccurrences)
+    bold = me.DictField()
+    capital_required = me.DictField()
+    cross_references = me.DictField()
+    exclusiveness = me.DictField()
+    inline_audio = me.DictField()
+    italics = me.DictField()
+    main_vocab = me.BooleanField()
+    meanings = me.DictField()
     meta = {'abstract': True}
-    minor_vocab = BooleanField()
-    orthoepy = DictField()
-    orthography = EmbeddedDocumentField(Orthography)
-    part_of_speech = StringField()
-    phrases = DictField()
-    quotations = DictField()
-    subjects = DictField()
-    transliterations = DictField()
+    minor_vocab = me.BooleanField()
+    orthoepy = me.DictField()
+    orthography = me.EmbeddedDocumentField(Orthography)
+    part_of_speech = me.StringField()
+    phrases = me.DictField()
+    quotations = me.DictField()
+    subjects = me.DictField()
+    transliterations = me.DictField()
 
 class A(BaseWordDocument):
     collection = "a"
