608dd680375d927946b0072e5e43cb726414776e
Zachary Flohr <zachary.flohr@case.edu>
2024-01-01 11:58:10 -0500

    Install Debian dependencies if missing

--- infrastructure/nginx/install_upgrade_nginx.sh
+++ infrastructure/nginx/install_upgrade_nginx.sh
@@ -7,7 +7,32 @@ tar -zxf ~/Downloads/nginx-*tar.gz -C ~/Downloads;
 rm ~/Downloads/nginx-*tar.gz;
 sudo apt-get update;
 sudo apt-get upgrade;
+
+# Install dependencies if missing.
+dpkg --get-selections | grep "[[:blank:]]install" \
+    | grep "libpcre2-dev[:[:blank:]]" > /dev/null \
+    || sudo apt-get install libpcre2-dev;
+dpkg --get-selections | grep "[[:blank:]]install" \
+    | grep "libpcre2-posix3[:[:blank:]]" > /dev/null \
+    || sudo apt-get install libpcre2-posix3;
+dpkg --get-selections | grep "[[:blank:]]install" \
+    | grep "zlib1g-dev[:[:blank:]]" > /dev/null \
+    || sudo apt-get install zlib1g-dev;
+dpkg --get-selections | grep "[[:blank:]]install" \
+    | grep "zlib1g[:[:blank:]]" > /dev/null \
+    || sudo apt-get install zlib1g;
+dpkg --get-selections | grep "[[:blank:]]install" \
+    | grep "openssl[:[:blank:]]" > /dev/null \
+    || sudo apt-get install openssl;
+dpkg --get-selections | grep "[[:blank:]]install" \
+    | grep "libssl-dev[:[:blank:]]" > /dev/null \
+    || sudo apt-get install libssl-dev;
+dpkg --get-selections | grep "[[:blank:]]install" \
+    | grep "gcc[:[:blank:]]" > /dev/null \
+    || sudo apt-get install gcc;
+
 sudo apt-get autoremove;
+sudo apt-get clean;
 cd ~/Downloads/nginx-*;
 
 ./configure \
@@ -22,10 +47,12 @@ sudo make install;
 cd ..;
 rm -r nginx-*;
 
+# Create symbolic link to nginx binary if missing.
 [ -L /usr/local/bin/nginx ] \
     || sudo ln -s /usr/local/nginx/sbin/nginx /usr/local/bin/nginx;
 
-awk -F : '{print $1}' /etc/passwd | grep 'nginx' > /dev/null \
+# Create nginx user if missing.
+awk -F : '{print $1}' /etc/passwd | grep "nginx" > /dev/null \
     || sudo useradd -r nginx;
 
 sudo chown -R nginx /usr/local/nginx/logs;
