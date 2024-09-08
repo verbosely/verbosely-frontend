1feef2f04c7956339ad23684d54710499182ec7b
Zachary Flohr <zachary.flohr@case.edu>
2024-09-08 05:19:19 -0400

    Check for needed binaries and for root privilege

--- shell-scripts/llvm.sh
+++ shell-scripts/llvm.sh
@@ -1,10 +1,32 @@
+#!/usr/bin/env bash
+
+check_root_user() {
+    [ ${EUID} -ne 0 ] && echo "This script must be run as root!" >&2 && exit 1
+}
+
+check_binaries() {
+    needed_binaries=(lsb_release awk dpkg grep
+                     wget add-apt-repository tee apt-get)
+    missing_binaries=()
+    for binary in "${needed_binaries[@]}"; do
+        which ${binary} &> /dev/null || missing_binaries+=($binary)
+    done
+    [ ${#missing_binaries[@]} -gt 0 ] \
+        && echo "You must install the following tools to run this script: \
+            ${missing_binaries[@]}" >&2 \
+        && exit 1
+}
+
+check_root_user
+check_binaries
+
 LLVM_VERSION=18
 BASE_URL="http://apt.llvm.org"
 CODENAME=$(lsb_release -c | awk '{ print $NF }')
 CURRENT_VERSION=$(dpkg --get-selections | grep "[[:blank:]]install$" \
     | grep ^clang-[[:digit:]] | awk '{ print $1 }' \
     | awk 'BEGIN { FS = "[^0-9]" }; { print $NF }')
- 
+
 packages() {
     echo "clang-$1 lldb-$1 lld-$1 clangd-$1"
 }
