bcecaf638ca82584d358559c386071da81a6fdf7
Zachary Flohr <zachary.flohr@case.edu>
2024-09-10 06:53:02 -0400

    Add functions for printing usage and parsing arguments

--- shell-scripts/llvm.sh
+++ shell-scripts/llvm.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
+LLVM_VERSION=18
+
+usage() {
+    cat <<- USAGE
+Usage: ./$(basename "${0}") [OPTION]
+    
+Summary:
+    Install the LLVM tools needed to build Python with
+    --enable-optimizations and --with-lto configure options. The major
+    version to install is specified in the shell parameter
+    LLVM_VERSION (=${LLVM_VERSION}) at the top of the script. If a
+    different major version is already installed on the system, then
+    that version will be purged before the version that LLVM_VERSION
+    expands to is installed.
+
+    The following LLVM tools will be installed:
+    clang: an "LLVM native" C/C++/Objective-C compiler
+    lldb: a C/C++/Objective-C debugger
+    lld: the LLVM linker
+
+    Refer here for more info: https://llvm.org
+
+Options:
+    -h | --help     display this help text, and exit
+USAGE
+}
+
 check_root_user() {
     [ ${EUID} -ne 0 ] && echo "This script must be run as root!" >&2 && exit 1
 }
@@ -17,10 +44,33 @@ check_binaries() {
         && exit 1
 }
 
+parse_args() {
+    TEMP=$(getopt -o 'h' --long 'help' -n $(basename "${0}") -- "$@")
+    getopt_exit_status=$?
+    [ ${getopt_exit_status} -ne 0 ] \
+        && echo 'Terminating...' >&2 \
+        && exit ${getopt_exit_status}
+    eval set -- "$TEMP"
+    unset TEMP
+    while true; do
+        case "$1" in
+            '-h'|'--help')
+                usage
+                exit 0
+            ;;
+            '--')
+                shift
+                break
+            ;;
+        esac
+    done
+    [ $# -ne 0 ] && usage >&2 && exit 1
+}
+
+parse_args $*
 check_root_user
 check_binaries
 
-LLVM_VERSION=18
 BASE_URL="http://apt.llvm.org"
 CODENAME=$(lsb_release -c | awk '{ print $NF }')
 CURRENT_VERSION=$(dpkg --get-selections | grep "[[:blank:]]install$" \
@@ -28,7 +78,7 @@ CURRENT_VERSION=$(dpkg --get-selections | grep "[[:blank:]]install$" \
     | awk 'BEGIN { FS = "[^0-9]" }; { print $NF }')
 
 packages() {
-    echo "clang-$1 lldb-$1 lld-$1 clangd-$1"
+    echo "clang-$1 lldb-$1 lld-$1"
 }
 
 repo() {
