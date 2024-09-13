2566945a3276b4a655856205e4580a6661a14445
Zachary Flohr <zachary.flohr@case.edu>
2024-09-13 07:21:51 -0400

    Write OpenPGP public key to proper location
    - /etc/apt/trusted.gpg and /etc/apt/trusted.gpg.d are unsecure locations
      for public keys; refer to apt-key(8) reference manual.
    - Public keys managed by packages should go in /usr/share/keyrings;
      refer to sources.list(5) manual.

--- shell-scripts/llvm.sh
+++ shell-scripts/llvm.sh
@@ -32,8 +32,7 @@ check_root_user() {
 }
 
 check_binaries() {
-    needed_binaries=(lsb_release awk dpkg grep basename getopt
-                     wget add-apt-repository tee apt-get)
+    needed_binaries=(lsb_release awk dpkg grep gpg getopt wget apt-get)
     missing_binaries=()
     for binary in "${needed_binaries[@]}"; do
         which ${binary} &> /dev/null || missing_binaries+=($binary)
@@ -72,38 +71,47 @@ parse_args $*
 check_root_user
 
 BASE_URL="http://apt.llvm.org"
+PPA_DIR="/etc/apt/sources.list.d"
+GPG_DIR="/usr/share/keyrings"
+LLVM_GPG_BASENAME="apt.llvm.org.gpg"
 CODENAME=$(lsb_release -c | awk '{ print $NF }')
 CURRENT_VERSION=$(dpkg --get-selections | grep "[[:blank:]]install$" \
     | grep ^clang-[[:digit:]] | awk '{ print $1 }' \
     | awk 'BEGIN { FS = "[^0-9]" }; { print $NF }')
+CURRENT_LLVM_SOURCE_FILE="llvm-${CURRENT_VERSION}.list"
+LLVM_SOURCE_FILE="llvm-${LLVM_VERSION}.list"
 
 packages() {
     echo "clang-$1 lldb-$1 lld-$1"
 }
 
 repo() {
-    echo "deb ${BASE_URL}/${CODENAME}/ llvm-toolchain-${CODENAME}-$1 main"
+    echo deb [arch=amd64 signed-by=${GPG_DIR}/${LLVM_GPG_BASENAME}] \
+        ${BASE_URL}/${CODENAME}/ llvm-toolchain-${CODENAME}-$1 main
 }
 
 install_llvm() {
     PKGS=$(packages ${LLVM_VERSION})
     REPO=$(repo ${LLVM_VERSION})
-    [ ! -f /etc/apt/trusted.gpg.d/apt.llvm.org.asc ] \
-        && wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key \
-            | tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
-    add-apt-repository -L | grep "${REPO}" 1> /dev/null \
-        || add-apt-repository -y "${REPO}"
+    [ -f ${GPG_DIR}/${LLVM_GPG_BASENAME} ] \
+        || wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key \
+            | gpg -o ${GPG_DIR}/${LLVM_GPG_BASENAME} --dearmor \
+        && chmod 0644 ${GPG_DIR}/${LLVM_GPG_BASENAME}
+
+    [ -f ${PPA_DIR}/${LLVM_SOURCE_FILE} ] \
+        || bash -c "echo ${REPO} > ${PPA_DIR}/${LLVM_SOURCE_FILE}"
     apt-get update
     apt-get -y install ${PKGS}
+    apt-get -y autoremove
 }
 
 uninstall_llvm() {
     PKGS=$(packages ${CURRENT_VERSION})
     REPO=$(repo ${CURRENT_VERSION})
     [ ${CURRENT_VERSION} ] && [ ${CURRENT_VERSION} != ${LLVM_VERSION} ] \
-        && apt-get -y purge ${PKGS} && apt-get -y autoremove \
-        && add-apt-repository -L | grep "${REPO}" 1> /dev/null \
-        && add-apt-repository -r -y "${REPO}"
+        && apt-get -y purge ${PKGS} \
+        && [ -f ${PPA_DIR}/${CURRENT_LLVM_SOURCE_FILE} ] \
+        && rm ${PPA_DIR}/${CURRENT_LLVM_SOURCE_FILE}
 }
 
 uninstall_llvm
