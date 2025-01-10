91b6a4b632d2445a22de6c8c5c749ff0b48e665b
Zachary Flohr <zachary.flohr@case.edu>
2025-01-10 14:42:50 -0500

    Remove shell-scripts/ directory

---
+++ git-configs/git_configs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#
+# Author: Zachary Flohr
+
+git config --local core.hooksPath git-hooks
--- shell-scripts/git-configs/git_configs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#
-# Author: Zachary Flohr
-
-git config --local core.hooksPath shell-scripts/git-hooks
